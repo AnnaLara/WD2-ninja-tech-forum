844c0cfed6208953873b8d84796a42a7ae546319
Anna Zubova <anna@leolog.com>
2018-06-04 20:20:33 +0200

    Classwork: add CSRF protection and memcache

--- handlers/add_topic.py
+++ handlers/add_topic.py
@@ -1,17 +1,40 @@
-from google.appengine.api import users
+from google.appengine.api import users, memcache
+
+import uuid
 
 from handlers.base import BaseHandler
 from models.topic import Topic
 
 class TopicAddHandler(BaseHandler):
     def get(self):
-        return self.render_template("topic_add.html")
-
-    def post(self):
         logged_user = users.get_current_user()
 
         if not logged_user:
             return self.write("Please, login first")
+
+        csrf_token = str(uuid.uuid4())
+        memcache.add(key=csrf_token, value=logged_user.email(), time=600)
+
+        context = {
+            "csrf_token": csrf_token,
+        }
+
+        return self.render_template("topic_add.html", params = context)
+
+    def post(self):
+
+        logged_user = users.get_current_user()
+
+        if not logged_user:
+            return self.write("Please, login first")
+
+        csrf_token = self.request.get('csrf-token')
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or mem_token != logged_user.email():
+            return self.write("This website is protected against CSRF")
+
+
 
         title_value = self.request.get("title")
         text_value = self.request.get("text")
@@ -29,6 +52,5 @@ class TopicAddHandler(BaseHandler):
         )
 
         new_topic.put()
-        topic = Topic.query(Topic.title == new_topic.title, Topic.content == new_topic.content, Topic.author_email == new_topic.author_email).fetch()
 
-        return self.redirect("/topics/topic.key.id()")
+        return self.redirect_to("topic-details", topic_id=new_topic.key.id())
